6cf015cc2333e832bb4a36399ccadb8c74b570f1
Six <23470032+6ixfalls@users.noreply.github.com>
2023-10-01 20:54:28 -0700

    Fix offlineStatus

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const config = {
   watchNamespace: process.env.WATCH_NAMESPACE || "default",
   annotationPrefix: process.env.ANNOTATION_PREFIX || "infrared-scaler.sixfal.ls",
   infraredUrl: process.env.INFRARED_URL || "http://infrared:8080",
-  offlineStatus: JSON.parse(process.env.OFFLINE_STATUS || `{ maxPlayers: 10, playersOnline: 0, motd: "\u00A78This server is asleep. Connect to start." }`),
+  offlineStatus: JSON.parse(process.env.OFFLINE_STATUS || `{ "maxPlayers": 10, "playersOnline": 0, "motd": "\u00A78This server is asleep. Connect to start." }`),
   serverUrl: process.env.SCALER_URL || "http://infrared-scaler:3000",
 }
 
